e360f0a4d2002c1cab1ba400d97fdd5a4412715f
Fanatic-Coder <GXenakis2@gmail.com>
2019-04-12 04:04:40 +0300

    Rearranged code for PEP 8 syntax corrections

--- 100yearOld/100yearOld.py
+++ 100yearOld/100yearOld.py
@@ -6,20 +6,21 @@ lname = input("Enter your last name: ")
 yearsstill = int()
 yearof100 = int()
 yearsago = int()
+# I can ask the user to input his birth date and calculate his age
 age = int(input("Enter your age: "))
-#I can ask the user to input his birth date and calculate his age
 
-if(age < 100):
-    yearsstill = 100 - age  #How many years you got untill you get 100 years old
-    yearof100 = 2018 + yearsstill   #What year you ll be closing 100 years old
+if age < 100:
+    yearsstill = 100 - age  # How many years you got untill you get 100 years old
+    yearof100 = 2018 + yearsstill   # What year you ll be closing 100 years old
     print("You are not 100 years old yet. \nYou still got " + str(yearsstill) + " years left!")
     print("You'll be 100 years old at " + str(yearof100) + "!")
-elif(age == 100):
+elif age == 100:
     print("You 've just Reached 100 Years old!")
-elif(age > 100): #When you are beyond 100 years old
+elif age > 100:  # When you are beyond 100 years old
     yearsago = age - 100
     yearof100 = 2018 - 100
     print("Well Done! You 're beyond 100 years old!")
-    print("You Closed 100 years old " + str(yearsago) + " years ago! And the year was " + str(yearof100) + "! Have a Long Life!")
+    print("You Closed 100 years old " + str(yearsago) + " years ago!"
+                                                        " And the year was " + str(yearof100) + "! Have a Long Life!")
 
 pause_exit(0, "Press any key to exit...")
