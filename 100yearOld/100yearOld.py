dd36747ff14e6eb9cd885e3bb1d87f346b9d88db
Fanatic-Coder <GXenakis2@gmail.com>
2019-04-12 04:09:46 +0300

    Corrected comments.

--- 100yearOld/100yearOld.py
+++ 100yearOld/100yearOld.py
@@ -6,17 +6,17 @@ lname = input("Enter your last name: ")
 yearsstill = int()
 yearof100 = int()
 yearsago = int()
-# I can ask the user to input his birth date and calculate his age
+# Asks the user to input his/her birth date and calculate his/her age
 age = int(input("Enter your age: "))
 
 if age < 100:
-    yearsstill = 100 - age  # How many years you got untill you get 100 years old
-    yearof100 = 2018 + yearsstill   # What year you ll be closing 100 years old
+    yearsstill = 100 - age  # How many years user got until he/she gets 100 years old
+    yearof100 = 2018 + yearsstill   # What year user will be closing 100 years old
     print("You are not 100 years old yet. \nYou still got " + str(yearsstill) + " years left!")
     print("You'll be 100 years old at " + str(yearof100) + "!")
 elif age == 100:
     print("You 've just Reached 100 Years old!")
-elif age > 100:  # When you are beyond 100 years old
+elif age > 100:  # When user is beyond 100 years old
     yearsago = age - 100
     yearof100 = 2018 - 100
     print("Well Done! You 're beyond 100 years old!")
